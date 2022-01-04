303a82c14b97b13cd78fa301d34a16aea336b083
Osvaldo De la torre <ekobozz9@gmail.com>
2022-01-04 13:34:07 -0600

    Se agrego el metodo de consultaEmp

--- scripts/php/modelo/empleado_dao.php
+++ scripts/php/modelo/empleado_dao.php
@@ -23,6 +23,25 @@ class EmpleadoDAO{
         return $res;
         
     }
+
+    public function modificarEmp($emp_no, $birth_date, $first_name,$last_name, $gender, $hire_date){
+        
+        $sql = "UPDATE Empleados SET birth_date = '$birth_date',first_name = '$first_name',last_name = '$last_name',gender ='$gender',hire_date='$hire_date' WHERE emp_no = '$emp_no'";
+        $res = mysqli_query($this->conexion->getConexion(), $sql);
+        return $res;
+        
+    }
+
+    //Select * from Empleados where emp_no LIKE '%" + $emp_no + "%' AND birth_date LIKE '%" + $birth_date + "%' AND first_name LIKE '%" + sa + "%' AND last_name LIKE '%" + te + "%' AND gender LIKE '%" + ti + "%' AND hire_date LIKE '%" + re + "%';";
+
+    public function Emp($emp_no, $birth_date, $first_name,$last_name, $gender, $hire_date){
+        
+        $sql = "UPDATE Empleados SET birth_date = '$birth_date',first_name = '$first_name',last_name = '$last_name',gender ='$gender',hire_date='$hire_date' WHERE emp_no = '$emp_no'";
+        $res = mysqli_query($this->conexion->getConexion(), $sql);
+        return $res;
+        
+    }
+
 }
 
 ?>
