5f368c44b04f520a50e0956a83e148215132814a
Osvaldo De la torre <ekobozz9@gmail.com>
2022-01-04 11:29:49 -0600

    Se corrijio el metodo agregarEmp

--- scripts/php/modelo/empleado_dao.php
+++ scripts/php/modelo/empleado_dao.php
@@ -10,7 +10,7 @@ class EmpleadoDAO{
 
     public function agregarEmp($emp_no, $birth_date, $first_name,$last_name, $gender, $hire_date){
         
-        $sql = "INSERT INTO Usuarios values ('$emp_no','$birth_date','$first_name','$last_name','$gender','$hire_date')";
+        $sql = "INSERT INTO Empleados values ('$emp_no','$birth_date','$first_name','$last_name','$gender','$hire_date')";
         $res = mysqli_query($this->conexion->getConexion(), $sql);
         return $res;
         
