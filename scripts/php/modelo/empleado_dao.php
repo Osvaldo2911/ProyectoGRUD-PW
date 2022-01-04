a377fa3c07466d738defbb39f2fccd5e09bcfe7e
Osvaldo De la torre <ekobozz9@gmail.com>
2022-01-04 11:20:04 -0600

    Se agrego el metodo agregarEmp

---
+++ scripts/php/modelo/empleado_dao.php
@@ -0,0 +1,20 @@
+<?php
+
+include('../conexionbd/conexionbd_empleados.php');
+
+class EmpleadoDAO{
+    private $conexion;
+    public function __construct(){
+        $this->conexion=new ConexionBDEmpresa();
+    }
+
+    public function agregarEmp($emp_no, $birth_date, $first_name,$last_name, $gender, $hire_date){
+        
+        $sql = "INSERT INTO Usuarios values ('$emp_no','$birth_date','$first_name','$last_name','$gender','$hire_date')";
+        $res = mysqli_query($this->conexion->getConexion(), $sql);
+        return $res;
+        
+    }
+}
+
+?>
